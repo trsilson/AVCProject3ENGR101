0708d490207a6c6c126a281cbc34dcacebc4e7b0
Rosanne <rosanne.sap@gmail.com>
2020-06-13 18:08:09 +1200

    edited and commented code

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,62 +1,58 @@
-
 #include "robot.hpp"
-#include <iostream>
 
 int main(){
 	if (initClientRobot()!=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 10;
-    double vRight = 10;
-    double vBaseLine = 10;
-    double kP = 0.03;
+  /* variables */
+  double vBaseLine = 10; // crusing speed if no errors
+  double vLeft = vBaseLine, vRight = vBaseLine; // left and right speed initialised to cruising speed
+  double kP = 0.03; // constant for difference in left and right speed - dv
 
-    while(1){
-	    takePicture();
-		SavePPMFile("i0.ppm",cameraView);
-		setMotors(vLeft,vRight);   
-		std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-		usleep(10000);
+  while(1){
+    takePicture();
+    SavePPMFile("i0.ppm",cameraView);
+    setMotors(vLeft,vRight);   
+    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+    usleep(10000);
 		
-		int collectPix[cameraView.width];
-		
-		
-		for(int i = 0; i < cameraView.height; i++) {
-			for(int j = 0; j < cameraView.width; j++) {
-				int lum = get_pixel(cameraView, i, j, 3);
+    int collectPix[cameraView.width]; // create an array with a size of cameraView.width
+      
+    for(int i = 0; i < cameraView.height; i++) { // scanning rows
+      for(int j = 0; j < cameraView.width; j++) { // scanning columns
+        /* calculate pixel luminosity */
+        int lum = get_pixel(cameraView, i, j, 3); 
+        /* if luminosity is > 250 then it's most likely a white pixel is found */
+        if(lum > 250) {
+        collectPix[j] = 1; // so add 1 to the array
+        } else {
+        collectPix[j] = 0; // or else add 0 to the array
+        }
+      }
+    }
+      
+    double error = 0; 
+      for(int i = 0; i < cameraView.width; i++) {
+      if(collectPix[i] == 1) {
+        error = error + (i - (cameraView.width / 2)); // calculate error
+      }
+    }
 
-				if(lum > 250) {
-					collectPix[j] = 1;
-				} else {
-					collectPix[j] = 0;
-				}
-				
-			}
-		}
-		
-		double error = 0;
-		for(int i = 0; i < cameraView.width; i++) {
-			if(collectPix[i] == 1) {
-				error = error + (i - (cameraView.width / 2));
-				std::cout<<" error = "<<error<<std::endl;
-			}
-		}
+    /* if white pixels are on the right, increase right speed so robot moves to the left */
+    if (error > 0) { 
+      vLeft = vBaseLine;
+      vRight = vBaseLine - error * kP; 
+    /* if white pixels are on the left, increase left motor so robot moves to the right */
+    } else if (error < 0) {
+      vLeft = vBaseLine  + error * kP;
+      vRight = vBaseLine; 
+    }  
+    /* else have a steady speed */
+    else {
+      vLeft = vBaseLine + error * kP;
+      vRight = vBaseLine - error * kP; 
+    }
 
-    if ( error > 0 ) { // if the white pixels is on right increase right motor
-        vLeft = vBaseLine;
-        vRight = vBaseLine - error * kP; 
-		    std::cout<<" increasing right motor"<<std::endl;
-	  } else if (error < 0 ) { // if the white pixels is on left increase left motor
-        vLeft = vBaseLine  + error * kP;
-        vRight = vBaseLine; 
-        std::cout<<" increasing left motor"<<std::endl;
-	  } else { // else have steady speed
-        vLeft = vBaseLine;
-        vRight = vBaseLine; 
-        std::cout<<" steady speed"<<std::endl;
-	  }
-
-		
   } //while
 
 } // main
