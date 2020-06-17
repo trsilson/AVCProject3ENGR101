93121e80f6b5883dfe319cd0ec413458e081cab1
Rosanne <rosanne.sap@gmail.com>
2020-06-17 22:26:42 +1200

    updated method core

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -2,15 +2,6 @@
 #include <vector>
 #include <fstream>
 
-
-
- class something{
-    public:
-    void doCore(){
-
-    }
-  };
-
 /*This class represents a line in the camera view which we scan for white pixels on
  *This class contructor can be used to create multiple scanline objects, and we can easily check these objects to see if they
  * Have detected a white line and what the error is from the center of that scan line
@@ -130,83 +121,39 @@ bool ScanLine::checkWhite(){
 double vBaseLine = 10; // crusing speed if no errors
 double vLeft = vBaseLine, vRight = vBaseLine; // left and right speed initialised to cruising speed
 
-/* function to calculate error */
-double calculateError(){ 
-  OpenPPMFile("cameraShot.png", cameraView);
-  SavePPMFile("currPic.ppm", cameraView);
-
-  int collectPix[cameraView.width]; // create an array with a size of cameraView.width
-    
-  for(int i = 0; i < cameraView.height; i++) { // scanning rows
-    for(int j = 0; j < cameraView.width; j++) { // scanning columns
-      /* calculate pixel luminosity */
-      int lum = get_pixel(cameraView, i, j, 3); 
-      /* if luminosity is > 250 then it's most likely a white pixel is found */
-      if(lum > 250) {
-      collectPix[j] = 1; // so add 1 to the array
-      } else {
-      collectPix[j] = 0; // or else add 0 to the array
-      }
-    }
-  }
-
-  double error = 0; 
-  for(int i = 0; i < cameraView.width; i++) {
-    if(collectPix[i] == 1) {
-      error = error + (i - (cameraView.width / 2)); // calculate error
-    }
-  }
-    
-  return error; // returning the calculated error
-}
-
-/* method to move robot using the calculate error function */
-void moveRobot() {
-  //double marginOfError = calculateError(); // setting the calculate error to the variable marginOfError
-   ScanLine bottomView = ScanLine("row", cameraView.height - 5);
-   bottomView.update(cameraView);
-   double marginOfError = bottomView.getError();
-
-  double kP = 0.03; // constant for difference in left and right speed - dv
-
-  /* if white pixels are on the right, increase right speed so robot moves to the left */
-  if (marginOfError > 0) { 
-    vLeft = vBaseLine;
-    vRight = vBaseLine - marginOfError * kP; 
-  /* if white pixels are on the left, increase left motor so robot moves to the right */
-  } else if (marginOfError < 0) {
-    vLeft = vBaseLine  + marginOfError * kP;
-    vRight = vBaseLine; 
-  }  
-  /* else have a steady speed */
-  else {
-    vLeft = vBaseLine + marginOfError * kP;
-    vRight = vBaseLine - marginOfError * kP; 
-  }
-
-  setMotors(vLeft,vRight);   
-  std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
-}
-
 
 void doCore() {
   if (initClientRobot()!=0){
 		std::cout<<" Error initializing robot"<<std::endl;
-    
-
 	}
 
+  ScanLine bottomLine = ScanLine("row", (cameraView.height - 5));
 
   while(1){
+
     takePicture();
-    OpenPPMFile("cameraShot.png", cameraView);
-    SavePPMFile("i0.ppm",cameraView);
+    bottomLine.update(cameraView);
+    double marginOfError = bottomLine.getError();
+    double kP = 0.03;
 
+    /* if white pixels are on the right, increase right speed so robot moves to the left */
+    if (marginOfError > 0) { 
+      vLeft = vBaseLine;
+      vRight = vBaseLine - marginOfError * kP; 
+    /* if white pixels are on the left, increase left motor so robot moves to the right */
+    } else if (marginOfError < 0) {
+      vLeft = vBaseLine  + marginOfError * kP;
+      vRight = vBaseLine; 
+    }  
+    /* else have a steady speed */
+    else {
+      vLeft = vBaseLine + marginOfError * kP;
+      vRight = vBaseLine - marginOfError * kP; 
+    }
 
+    setMotors(vLeft,vRight);   
+    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 
-    moveRobot(); // calling the moveRobot method function in order to move the robot
-
-    
     usleep(10000);
   } //while
 }
