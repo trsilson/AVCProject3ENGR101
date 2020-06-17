4b5b0621e65efc999034e478f2f0891f7600ef98
Lachlan Paulsen <fireballinator@gmail.com>
2020-06-17 20:24:42 +1200

    Core function finished. Completion getting there Also put in the ability for the user to choose core/completion/challenge

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,18 +1,8 @@
 #include "robot.hpp"
 #include <vector>
+#include <fstream>
 
 
-void doCore() {
-
-}
-
-void doCompletion() {
-
-}
-
-void doChallenge() {
-
-}
 
 /*This class represents a line in the camera view which we scan for white pixels on
  *This class contructor can be used to create multiple scanline objects, and we can easily check these objects to see if they
@@ -186,10 +176,14 @@ void moveRobot() {
     vLeft = vBaseLine + marginOfError * kP;
     vRight = vBaseLine - marginOfError * kP; 
   }
+
+  setMotors(vLeft,vRight);   
+  std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
 }
 
-int main(){
-	if (initClientRobot()!=0){
+
+void doCore() {
+  if (initClientRobot()!=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
 
@@ -203,9 +197,115 @@ int main(){
 
     moveRobot(); // calling the moveRobot method function in order to move the robot
 
-    setMotors(vLeft,vRight);   
-    std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+    
     usleep(10000);
   } //while
+}
+
+
+void updateScanners(ScanLine tL, ScanLine lL, ScanLine bL, ScanLine rL){
+  takePicture();
+  tL.update(cameraView);
+  lL.update(cameraView);
+  rL.update(cameraView);
+  bL.update(cameraView);
+}
+
+
+void doCompletion() {
+  if (initClientRobot()!=0){
+		std::cout<<" Error initializing robot"<<std::endl;
+	}
+
+  std::ofstream logFile; // Create an output stream for a log file
+  logFile.open("completion log.txt", std::ios::out);
+  logFile << "HAHAHAHA RONAHLDINO SOCCER!!!";
+  logFile.close();
+  ScanLine bottomLine = ScanLine("row", (cameraView.height - 5));
+  ScanLine leftLine = ScanLine("col", 5);
+  ScanLine rightLine = ScanLine("col", (cameraView.width - 5));
+  ScanLine topLine = ScanLine ("row", 5);
+
+  
+
+
+  while (1){
+    updateScanners(topLine, leftLine, bottomLine, rightLine);
+
+    double marginOfError = bottomLine.getError();
+
+    double kP = 0.03;
+
+  //This be the code for following a sraight line forward
+  if (bottomLine.checkWhite() && !(leftLine.checkWhite() || rightLine.checkWhite())){
+      /* if white pixels are on the right, increase right speed so robot moves to the left */
+    if (marginOfError > 0) { 
+      vLeft = vBaseLine;
+      vRight = vBaseLine - marginOfError * kP; 
+    /* if white pixels are on the left, increase left motor so robot moves to the right */
+    } else if (marginOfError < 0) {
+      vLeft = vBaseLine  + marginOfError * kP;
+      vRight = vBaseLine; 
+    }  
+    /* else have a steady speed */
+    else {
+      vLeft = vBaseLine + marginOfError * kP;
+      vRight = vBaseLine - marginOfError * kP; 
+    }
+    if (!(bottomLine.checkWhite())){
+      vLeft = 0;
+      vRight = 0;
+    }
+    setMotors(vLeft,vRight);   
+  }
+  else{
+    //Maintain heading until average side error is above 100
+    if (leftLine.getError() > 100 || rightLine.getError() > 100){
+      setMotors(0,0);
+
+    //Go Left first, then straight second and then right last:
+
+
+
+    }
+  }
+  std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
+  std::cout<<"Left white detected: " << leftLine.checkWhite() << " Center white detected: " << bottomLine.checkWhite() << " Right white detected: " << rightLine.checkWhite() << std::endl;
+  std::cout<<"Left error: " << leftLine.getError() << " Center error: " << bottomLine.getError() << " right error: " << rightLine.getError() << std::endl;
+  usleep(10000);
+  }
+}
+
+void doChallenge() {
+
+}
+
+
+
+int main(){
+  bool choiceMade = false;
+  while (!(choiceMade)){
+  std::string choice;
+  std::cout<<"Which part of the code will you like to run [1 for core | 2 for completion | 3 for challenge]";
+  std::cin >> choice;
+  if (choice == "1"){
+    std::cout << std::endl << "RUNNING CORE" << std::endl;
+    choiceMade = true;
+    doCore();
+  }
+  else if (choice == "2"){
+    std::cout << std::endl << "RUNNING COMPLETION" << std::endl;
+    choiceMade = true;
+    doCompletion();
+  }
+  else if (choice == "3"){
+    std::cout << std::endl << "RUNNING CHALLENGE" << std::endl;
+    choiceMade = true;
+    doChallenge();
+  }
+  else {
+    std::cout << "WRONG! Try again" <<std::endl;
+  }
+  }
 
 } // main
